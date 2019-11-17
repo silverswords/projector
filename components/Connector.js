812350e06d3aa5032ed5bc048929a86c15df0fc6
abser <abserari@gmail.com>
2019-11-17 23:23:06 +0800

    add queue

--- components/Connector.js
+++ components/Connector.js
@@ -1,32 +1,15 @@
-import { useEffect, useReducer } from "react"
+import { useEffect, useState } from "react"
 // Design
 // Action = Endpoint
 // Decorators = Middleware
+const Connector
+const connect = (name) => (
+    [state, setState] = useState(name)
 
-const Store = {}
+    useEffect(
+        Connector.subscribe(name,setState)
+        return () =>{
 
-const ActionSet = new Set()
-
-export function useStore(key,reducer,initialState){
-    const [state, dispatch] = useReducer(reducer, initialState)
-
-    useEffect(() => {
-        Store[key] = state
-      }, [key, state]);
-
-    return [state,dispatch]
-}
-
-// use =====================================
-const initialState = {count: 0};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case 'increment':
-      return {count: state.count + 1};
-    case 'decrement':
-      return {count: state.count - 1};
-    default:
-      throw new Error();
-  }
-}
+        }
+    )
+)
