dfb9101e0b91c9902faed971886d95bc26e7f925
abser <abserari@gmail.com>
2019-11-18 00:34:35 +0800

    add queue event listener as Connector

--- components/Connector.js
+++ components/Connector.js
@@ -2,14 +2,28 @@ import { useEffect, useState } from "react"
 // Design
 // Action = Endpoint
 // Decorators = Middleware
-const Connector
-const connect = (name) => (
+const Connector = {}
+
+const Broadcast = (name, state) => {
+    if (!Connector[name]) return;
+    Connector[name].forEach(setter => setter(state))
+}
+
+const Subscribe = (name, setter) => {
+    if (!Connector[name]) Connector[name] =[];
+    Connector[name].push(setter)
+}
+
+const unSubscribe = (name, setter) => {
+    if (!Connector[name]) return
+    const index = Connector[name].indexOf(setter)
+    if (index !== -1) Connector[name].splice(index, 1)
+}
+
+const connect = (name) => {
     [state, setState] = useState(name)
-
-    useEffect(
-        Connector.subscribe(name,setState)
-        return () =>{
-
-        }
-    )
-)
+    useEffect(() =>{
+        Subscribe(name, setState)
+        return () => unSubscribe(name,setState)
+    },[])
+}
