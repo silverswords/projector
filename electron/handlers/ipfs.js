6259c35c933b2c745f7ea67e853c2b39a4734f1c
abser <abserari@gmail.com>
2019-11-23 14:03:19 +0800

    complete add listener to run ipfs

--- electron/handlers/ipfs.js
+++ electron/handlers/ipfs.js
@@ -1,9 +1,16 @@
 'use strict'
 
+const IPFS = require('ipfs')
 
-function onRunIPFS() {
-    console.log('start a node')
-    
+async function onRunIPFS()  {
+  console.log('waiting an IPFS Node')
+  try {
+    const node = await IPFS.create()
+    const id = await node.id()
+    console.log(id)
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 module.exports = {
--- electron/main.js
+++ electron/main.js
@@ -2,22 +2,14 @@ const { app } = require('electron')
 
 const Listener = require('./utility/listener')
 const { createWindow, onActivateWindow } = require('./handlers/window')
-const IPFS = require('ipfs')
+const { onRunIPFS } = require('./handlers/ipfs')
 const { onCreateMenu } = require('./menu')
 
 const listener = new Listener()
 listener.addListener('ready', createWindow)
 listener.addListener('ready', onCreateMenu)
 // create ipfs node in electron
-listener.addListener('ready', async () => {
-  try {
-    const node = await IPFS.create()
-    const id = await node.id()
-    console.log(id)
-  } catch (err) {
-    console.error(err)
-  }
-})
+listener.addListener('ready', onRunIPFS)
 listener.addListener('activate', onActivateWindow)
 listener.addListener('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
