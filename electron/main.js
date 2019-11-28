0991dda729d666cc32e1f70935a0e7151971eb99
abser <abserari@gmail.com>
2019-11-28 09:15:26 +0800

    delete old ipfsUI

--- electron/main.js
+++ electron/main.js
@@ -2,14 +2,11 @@ const { app } = require('electron')
 
 const Listener = require('./utility/listener')
 const { createWindow, onActivateWindow } = require('./handlers/window')
-const { onRunIPFS } = require('./handlers/ipfs')
 const { onCreateMenu } = require('./menu')
 
 const listener = new Listener()
 listener.addListener('ready', createWindow)
 listener.addListener('ready', onCreateMenu)
-// create ipfs node in electron
-listener.addListener('ready', onRunIPFS)
 listener.addListener('activate', onActivateWindow)
 listener.addListener('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
--- pages/ipfs.js
+++ pages/ipfs.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Head from 'next/head'
-
+import useIpfsFactory from '../components/ipfs/use-ipfs-factory'
+import useIpfs from '../components/ipfs/use-ipfs'
 
 const IPFSUI = () => {
-    
+  const { ipfs, ipfsInitError } = useIpfsFactory({ commands: ['id'] })
+  const id = useIpfs(ipfs, 'id')
+
     return (
     <div>
       <Head>
@@ -11,9 +14,45 @@ const IPFSUI = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <p>now check your console</p>
+      <header className='flex items-center pa3 bg-navy bb bw3 b--aqua'>
+        <a href='https://ipfs.io' title='home'>
+          <img alt='IPFS logo' src={logo} style={{ height: 50 }} className='v-top' />
+        </a>
+        <h1 className='flex-auto ma0 tr f3 fw2 montserrat aqua'>IPFS React</h1>
+      </header>
+      <main>
+        {ipfsInitError && (
+          <div className='bg-yellow pa4 mw7 center mv4 white'>
+            Error: {ipfsInitError.message || ipfsInitError}
+          </div>
+        )}
+        {id && <IpfsId {...id} />}
+      </main>
      </div>
     )
+}
+
+const Title = ({ children }) => {
+  return (
+    <h2 className='f5 ma0 pb2 tracked aqua fw4 montserrat'>{children}</h2>
+  )
+}
+
+const IpfsId = (props) => {
+  if (!props) return null
+  return (
+    <section className='bg-snow mw7 center mt5'>
+      <h1 className='f3 fw4 ma0 pv3 aqua montserrat tc' data-test='title'>Connected to IPFS</h1>
+      <div className='pa4'>
+        {['id', 'agentVersion'].map((key) => (
+          <div className='mb4' key={key}>
+            <Title>{key}</Title>
+            <div className='bg-white pa2 br2 truncate monospace' data-test={key}>{props[key]}</div>
+          </div>
+        ))}
+      </div>
+    </section>
+  )
 }
 
 export default IPFSUI
